ca14819568f3bcb4f074cc5aac9972d0ce075354
divyansh0777 <divyansh.mishra@successive.tech>
2019-04-20 12:38:44 +0530

    [Re] Fix : Fixes the content to display in airBnb style

--- extra/patterns/equilateralTriangle.js
+++ extra/patterns/equilateralTriangle.js
@@ -1,21 +1,18 @@
 let a = process.argv[2];
 
 let triangle = (value) => {
-   
-    for(let i=1;i<=value;i++)
-    {
-      let result = "";
-      for(let j=1 ; j<=value-i ; j++)
-      {
-          result = result + " ";
-      }
+  for (let i=1;i<=value;i++) {
+    let result = "";
 
-      for(let k=1;k<=i;k++)
-      {
-        result = result + "* ";
-      }
-     
-     console.log(result);
+    for ( let j=1; j<=value-i; j++ ) {
+      result = result + " ";
+    }
+
+    for (let k=1; k<=i; k++) {
+      result = result + "* ";
+    }
+      
+    console.log(result);
   }
 }
 
