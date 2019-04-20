444575786c76620a740057da2c3c28dd2876b01a
divyansh0777 <divyansh.mishra@successive.tech>
2019-04-20 13:03:51 +0530

    [Re] fix : fixes the regExp with apropriate string

--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -1,11 +1,11 @@
 const users = [     
   {
-    traineeEmail: 'trainee2@successive.tech',
-    reviewerEmail: 'reviewer2@successive.tech',
+    traineeEmail: 'trainee2@successive.tech@.tech', //FALSE
+    reviewerEmail: 'reviewer2@successive.tech',       
   },
 
   {
-    traineeEmail: 'trainee3@successsive.tech',
+    traineeEmail: 'trainee3@successsive.tech@.tech', //FALSE
     reviewerEmail: 'reviewer3@successive.tech',
   },
 
@@ -16,22 +16,22 @@ const users = [
 
   {
     traineeEmail: 'trainee5@successive.tech',
-    reviewerEmail: 'reviewer5@successsive.tech',
+    reviewerEmail: 'reviewer5@successive@successive.tech', //FALSE
   }
 ]
 
-let pattern = new RegExp ('@successive.tech');
-
+let pattern = new RegExp (/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/);
 //Validating Email of a user
 let validateEmail = (trainee , reviewer) => {
-    console.log(pattern.test(trainee));
-    console.log(pattern.test(reviewer));
+  console.log(pattern.test(trainee));
+  console.log(pattern.test(reviewer));
 }
+
 //Validating users 
 let validateUsers = (users) => {
-    console.log(users);
+  console.log(users);
   users.forEach((key) => {
-      console.log(key);
+    console.log(key);
     validateEmail(key.traineeEmail, key.reviewerEmail);
   })
 }
