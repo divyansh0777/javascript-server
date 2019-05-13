47193d03bbe115d58c6dca19ec9643cf3ed32db4
divyansh0777 <divyansh.mishra@successive.tech>
2019-05-13 13:52:45 +0530

    fix: conflicts

--- src/config/libs/Database.ts
+++ src/config/libs/Database.ts
@@ -1,22 +1,19 @@
 import * as mongoose from "mongoose";
 import { configure } from "../configuration";
-import { seedData } from "./seedData";
 
 class Database {
   public open = () => {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       const url = configure.mongoUrl;
 
       mongoose.connect(url, {useNewUrlParser: true});
 
       mongoose.connection.on("connected", () => {
-        seedData();
         console.log("Database connected");
         resolve();
       });
 
       mongoose.connection.on("error", () => {
-        console.log("Connection not established");
         reject();
       });
     });
