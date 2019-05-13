5364b162ca9617cdf12df00c7421e0ee91f76f9c
divyansh0777 <divyansh.mishra@successive.tech>
2019-05-13 12:38:51 +0530

    fix: conflicts

--- src/repositories/user/userModel.ts
+++ src/repositories/user/userModel.ts
@@ -1,11 +1,5 @@
 import * as mongoose from "mongoose";
-<<<<<<< HEAD
 import { IUser } from "./IUserModel";
 import userSchema from "./userSchema";
 
 export const user = mongoose.model<IUser>("user", userSchema);
-=======
-import schema from "./userSchema";
-
-export const user = mongoose.model("user", schema);
->>>>>>> develop
